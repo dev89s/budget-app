9685c82d6f210a86a33b5e5757ec6a0ba5c966c8
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-08-08 15:52:56 +0300

    Add authorization config

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,6 +6,8 @@ class Ability
   def initialize(user)
     # Define abilities for the user here. For example:
     #
+    return unless user.present?
+    can :manage, :all
     #   return unless user.present?
     #   can :read, :all
     #   return unless user.admin?
