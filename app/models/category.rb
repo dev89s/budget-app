b79a2212c1365b1ff54278529e1cdc6acf648119
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-08-11 18:30:30 +0300

    Add input validations for Category & Purchase models

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_and_belongs_to_many :purchases
 
-  validates :name, presence: true, length: { maximum: 100 }, format: { with: /\A[a-zA-Z][a-zA-Z0-9_\-\W]*\z/}
+  validates :name, presence: true, length: { maximum: 100 }, format: { with: /\A[a-zA-Z][a-zA-Z0-9_\-\W]*\z/ }
   validates :icon, presence: true
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,7 +2,7 @@ class Purchase < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_and_belongs_to_many :categories
 
-  validates :name, presence: true, length: { maximum: 100 }, format: { with: /\A[a-zA-Z][a-zA-Z0-9_\-\W]*\z/}
+  validates :name, presence: true, length: { maximum: 100 }, format: { with: /\A[a-zA-Z][a-zA-Z0-9_\-\W]*\z/ }
   validates :amount, presence: true, numericality: { only_number: true }
 
   validate :at_least_one_checkbox
